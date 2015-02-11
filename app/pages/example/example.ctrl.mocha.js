281a976c363cf3d2e04f84acf63e5f3e05cf0121
Neozaru <neozaru@mailoo.org>
2015-02-11 23:31:57 +0100

    Correct coverage in example controller

--- app/pages/example/example.ctrl.mocha.js
+++ app/pages/example/example.ctrl.mocha.js
@@ -4,11 +4,36 @@ define(['angular-mocks', 'exampleCtrl'], function(mocks) {
 
     var scope;
     var ctrl;
+    var q;
+    var location;
+
     var stubService;
+    var mockUsersService = {
+      ok: true,
+      activate: sinon.spy(function() {
+        _this = this;
+        return q(function(resolve, reject) {
+          if (_this.ok) {
+            resolve({userid: 1, token: "foo"});
+          }
+          else {
+            reject("Batman error");
+          }
+        });
+      })
+    };
+
+    var mockAuthenticationService = {
+      ok: true,
+      putLoginInfo: sinon.spy()
+    };
 
     beforeEach(mocks.module('myApp'));
 
-    beforeEach(inject(function($rootScope, $controller, $q) {
+    beforeEach(inject(function($rootScope, $controller, $q, $location) {
+      q = $q;
+      location = $location;
+      location.search({});
       scope = $rootScope.$new();
       stubService = {
         ok: true
@@ -24,8 +49,16 @@ define(['angular-mocks', 'exampleCtrl'], function(mocks) {
           }
         });
       };
-      
-      ctrl = $controller('exampleCtrl', {$scope: scope, 'exampleService': stubService});
+
+      mockAuthenticationService.putLoginInfo.reset();
+      mockUsersService.activate.reset();
+
+      ctrl = $controller('exampleCtrl', {
+        $scope: scope, 'exampleService': stubService, 
+        'usersService': mockUsersService, 
+        'authenticationService': mockAuthenticationService}
+      );
+
     }));
 
     it('should have initial values', function(){
@@ -61,8 +94,48 @@ define(['angular-mocks', 'exampleCtrl'], function(mocks) {
         expect(scope.lastError).to.equal("HTTP error : Batman error");
       });
 
-      it('should perform action', function() {
+      it('should not perform action if bad parameters', function() {
         scope.doAction();
+        expect(mockUsersService.activate).to.have.not.been.called;
+        location.search({action: "activate"});
+        scope.doAction();
+        expect(mockUsersService.activate).to.have.not.been.called;
+        location.search({userid: 1});
+        scope.doAction();
+        expect(mockUsersService.activate).to.have.not.been.called;
+        location.search({token: "foo"});
+        scope.doAction();
+        expect(mockUsersService.activate).to.have.not.been.called;
+        location.search({action: "activate", userid: 1});
+        scope.doAction();
+        expect(mockUsersService.activate).to.have.not.been.called;
+        location.search({action: "activate", token: "foo"});
+        scope.doAction();
+        expect(mockUsersService.activate).to.have.not.been.called;
+        location.search({userid: 1, token: "foo"});
+        scope.doAction();
+        expect(mockUsersService.activate).to.have.not.been.called;
+      });
+
+      it('should perform "activate" action if proper parameters', function() {
+        scope.onAccountActivated = sinon.spy(scope.onAccountActivated)
+        location.search({action: "activate", token: "foo", userid: 1});
+
+        {
+          scope.doAction();
+          expect(mockUsersService.activate).to.have.been.calledWith(1, "foo");
+          scope.$digest();
+          expect(scope.onAccountActivated).to.have.been.calledWith({userid: 1, token: "foo"});
+          scope.onAccountActivated.reset();
+        }
+
+        {
+          mockUsersService.ok = false;
+          scope.doAction();
+          scope.$digest();
+          expect(scope.onAccountActivated).to.have.not.been.called;
+          scope.onAccountActivated.reset();
+        }
       });
 
     });
